4f5b6747740ab74260d723b0b85bc8d63cd7915b
Александр <kalyghnii@gmail.com>
2022-08-04 09:20:02 +0500

    fix

--- src/main/java/hexlet/code/games/Gcd.java
+++ src/main/java/hexlet/code/games/Gcd.java
@@ -2,22 +2,22 @@ package hexlet.code.games;
 
 import hexlet.code.App;
 import hexlet.code.Engine;
+import hexlet.code.utils.Utils;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 
 public class Gcd {
+    private static final int MIN_NUM_RANDOM = 1;
     private static final int MAX_NUM_RANDOM = 50;
     private static final String DESCRIPTION = "Find the greatest common divisor of given numbers.";
-    private static Random rnd = new Random();
     public static void playingGame() {
         Map<String, String> conditions = new HashMap<>();
 
         for (var i = 0; i < App.MAX_QUANTITY_CORRECT_ANSWER; i++) {
-            int firstNumber = rnd.nextInt(MAX_NUM_RANDOM);
-            int secondNumber = rnd.nextInt(MAX_NUM_RANDOM);
+            int firstNumber = Utils.getNumberToInterval(MIN_NUM_RANDOM, MAX_NUM_RANDOM);
+            int secondNumber = Utils.getNumberToInterval(MIN_NUM_RANDOM, MAX_NUM_RANDOM);
 
             String question = firstNumber + " " + secondNumber;
             int correctAnswer = gettingCorrectAnswer(firstNumber, secondNumber);
